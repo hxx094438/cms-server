c437f3d641eb635cf98429ef636c6b412040f34a
hxx094438 <287668256@qq.com>
2018-04-16 06:22:45 +0800

    search

--- hxx/src/components/front/SearchResult.vue
+++ hxx/src/components/front/SearchResult.vue
@@ -7,12 +7,12 @@
             <span class="commentNumber"><i class="iconfont icon-huifu"></i>{{article.comment_n}}</span>
             <p>{{article.content}}</p>
             <router-link :to="{name: 'article', params: {id: article.aid, index: index, page: page}, hash: '#article'}" tag="button" exact>
-                <span>Continue reading</span>
+                <span><i>Read More</i></span>
             </router-link>
         </div>
         <spinner v-show="loadMore" class="loading"></spinner>
-        <p v-if="!loadMore" v-show="!noMore" class="noMore animated fadeIn">下拉加载更多</p>
-        <p v-if="noMore" class="noMore animated fadeIn">没啦没啦，别扯了</p>
+        <p v-if="!loadMore" @click="loadSearch" v-show="!noMore" class="noMore animated fadeIn"><i>more</i></p>
+        <p v-if="noMore" class="noMore animated fadeIn"><i>end</i></p>
     </div>
 </template>
 
@@ -53,15 +53,23 @@ export default {
     methods: {
         ...mapActions(['searchArticles']),
         ...mapMutations(['set_headline']),
-        handleScroll () {
-            if (!this.isLoading && this.$route.name === 'SearchResult') {
-                const body = document.body
-                const totalHeight = body.scrollHeight
-                const scrollTop = body.scrollTop
-                const clientHeight = window.innerHeight
-                if (totalHeight - scrollTop - clientHeight === 0 && this.moreArticle) {
-                    this.searchArticles({key: 'title', value: this.$route.params.text, add: true, page: ++this.page})
-                }
+//        handleScroll () {
+//            if (!this.isLoading && this.$route.name === 'SearchResult') {
+//                const body = document.body
+//                const totalHeight = body.scrollHeight
+//                const scrollTop = body.scrollTop
+//                const clientHeight = window.innerHeight
+//                if (totalHeight - scrollTop - clientHeight === 0 && this.moreArticle) {
+//                    this.searchArticles({key: 'title', value: this.$route.params.text, add: true, page: ++this.page})
+//                }
+//                if (!this.moreArticle) {
+//                    this.page = 1
+//                }
+//            }
+//        },
+        loadSearch () {
+            if (!this.isLoading && this.$route.name === 'search') {
+                this.searchArticles({key: 'title', value: this.$route.params.text, add: true, page: ++this.page})
                 if (!this.moreArticle) {
                     this.page = 1
                 }
@@ -79,15 +87,17 @@ export default {
 #search {
     padding: 0 12.5rem 0;
     div#article {
-        color: #fff;
+        color: #555;
         width: 100%;
-        border-bottom: 0.125rem solid rgb(129, 216, 208);
+        border-bottom: 1px solid #eee;
         h2 {
             margin-top: 1.875rem;
             margin-bottom: 1.25rem;
+            color: #333;
         }
         time {
             margin-top: 0.625rem;
+            color: #7f8c8d;
         }
         p {
             white-space: pre-wrap;
@@ -96,38 +106,38 @@ export default {
             margin-bottom: 1.875rem;
         }
         button {
-            width: 8.75rem;
-            height: 2.5rem;
-            line-height: 2.5rem;
-            background: rgb(129, 216, 208);
-            margin-bottom: 1.25rem;
-            border-radius: 0.25rem;
-            margin-left: calc(100% - 8.75rem);
+            margin-top: 0;
+            background: transparent;
+            font-weight: 600;
+            border-bottom: 1px solid #eee;
+            span{
+                color: #666;
+            }
         }
         .articleTag {
             margin-left: 1.25rem;
             margin-bottom: 1.875rem;
+            color: #7f8c8d;
         }
         .commentNumber {
             margin-left: 0.625rem;
-            color: #ffffff;
+            color: #666;
             i {
                 font-size: 1.125rem;
                 margin-right: 0.3125rem;
             }
         }
         i.icon-label, i.icon-shijian {
-            color: #ffffff;
+            color: #7f8c8d;
             font-size: 1.25rem;
             margin-right: 0.3125rem;
         }
     }
     p.noMore {
         width: 100%;
-        color: #ffffff;
+        color: #333;
         margin-top: 1.875rem;
         margin-bottom: 1.875rem;
-        text-align: center;
     }
 }
 @media screen and (max-width: 440px) {
