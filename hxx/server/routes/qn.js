82a12e79cbabe08bc1515931cdbeb20b8a23d946
huangxiaoxun <huangxiaoxun@xinli001.com>
2019-09-04 14:50:37 +0800

    获取七牛token

--- hxx/server/routes/qn.js
+++ hxx/server/routes/qn.js
@@ -2,19 +2,31 @@
  * @Author: huangxiaoxun 
  * @Date: 2019-09-04 10:21:32 
  * @Last Modified by: huangxiaoxun
- * @Last Modified time: 2019-09-04 10:27:03
+ * @Last Modified time: 2019-09-04 14:48:52
  */
 
  import{
      Get, Controller
  } from '../decorator/router'
- import qiniu from qiniu
-
- 
+ import key from '../config/key'
+ import qiniu from 'qiniu'
  @Controller('api/qiniu')
  export class QiniuRouter {
-     @Get('/save')
+     @Get('/')
      async getQiniuToken(ctx, next) {
-
+        const bucket = "hxx";  //存储空间名称
+        const mac = new qiniu.auth.digest.Mac(key.qn.ak, key.qn.sk);
+        const options = {
+            scope: bucket
+        }
+        const putPolicy = new qiniu.rs.PutPolicy(options);
+        const uploadToken = putPolicy.uploadToken(mac);
+        ctx.body = {
+            data: {
+                token: uploadToken
+            },
+            success: true,
+            code: 0
+          }
      }
  }
